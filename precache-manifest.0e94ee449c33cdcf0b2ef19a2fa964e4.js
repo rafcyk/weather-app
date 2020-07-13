9f6cc6646ce3d6acf2c9282e95991c5e796bd4fb
Rafcyk <rafcy9@gmail.com>
2020-07-13 13:58:33 +0200

    Updates

---
+++ precache-manifest.0e94ee449c33cdcf0b2ef19a2fa964e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364a96d7e91837b79d75ec4eaa3d34bc",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e2c33fb8df4f0a861559",
+    "url": "/weather-app/static/css/main.5d9559f6.chunk.css"
+  },
+  {
+    "revision": "b1e610564bb9f396c1a6",
+    "url": "/weather-app/static/js/2.01e6ea29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.01e6ea29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c33fb8df4f0a861559",
+    "url": "/weather-app/static/js/main.1213a857.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "614736612f3d0722ee92db23fa819307",
+    "url": "/weather-app/static/media/clouds-4215608_1920.61473661.jpg"
+  }
+]);
